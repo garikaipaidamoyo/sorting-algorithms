b3c9c2a21e9c5bfe70df516464d7ad4233dc5875
Paidamoyo Garikai <garikaipaidamoyo@gmail.com>
2023-09-01 03:13:25 -0700

    Bubble sort

---
+++ sort.h
@@ -0,0 +1,12 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
+/* Function prototypes */
+void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+void insertion_sort(int *array, size_t size);
+/* Add prototypes for other functions if needed */
+
+#endif /* SORT_H */
